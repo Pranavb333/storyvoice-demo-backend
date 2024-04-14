d6bf78e93618bb137cc7857031dbf05b013514ed
Pranav <44663510+Pranavb333@users.noreply.github.com>
2024-04-15 01:42:16 +0530

    Updated init script

--- scripts/init.py
+++ scripts/init.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 import motor
@@ -10,3 +11,6 @@ db = client[os.environ['DB_NAME']]
 
 db.users.create_index('email')
 db.stories.create_index('userID')
+
+Path('data/stories_audio').mkdir(parents=True, exist_ok=True)
+Path('data/reference_voice').mkdir(parents=True, exist_ok=True)
